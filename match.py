9204ca9a3463a487441a6e65a2076bc59ea727c9
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-05-17 16:23:52 +0530

    Update match.py

--- match.py
+++ match.py
@@ -125,11 +125,11 @@ df = df.loc[:,~df.columns.duplicated()]
 #df = df[['id','touchpointable_id','kind','title','name','createable_for_name','description']].copy()    
 df_T =  pd.merge(df, df_interest, left_on='name',right_on='Interest',suffixes=('', '_x'),how = 'inner')
 df_T = df_T.loc[:,~df_T.columns.duplicated()]
-df_T['idx'] = df_T.groupby(['touchpointable_id', 'name']).cumcount()
-df_T = df_T.pivot(index=['idx','touchpointable_id'], columns='name', values='Weight').sort_index(level=1).reset_index().rename_axis(None, axis=1)
-df_T.fillna(0)
-col_list = interest
-df_T['Sum'] = df_T[col_list].sum(axis=1)
+#df_T['idx'] = df_T.groupby(['touchpointable_id', 'name']).cumcount()
+#df_T = df_T.pivot(index=['idx','touchpointable_id'], columns='name', values='Weight').sort_index(level=1).reset_index().rename_axis(None, axis=1)
+#df_T.fillna(0)
+#col_list = interest
+#df_T['Sum'] = df_T[col_list].sum(axis=1)
 #df_T = pd.merge(df, df_T, left_on='touchpointable_id',right_on='touchpointable_id',suffixes=('', '_x'),how = 'inner')
 #df_T = df_T.loc[:,~df_T.columns.duplicated()]    
      #df_T =  pd.merge(df, df_interest, left_on='name',right_on='Interest',suffixes=('', '_x'),how = 'inner')
