cf347acf62352b089d147f7f8717dfbcc8a503c2
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-05-17 14:24:26 +0530

    Update match.py

--- match.py
+++ match.py
@@ -122,7 +122,7 @@ df = pd.merge(df,df_cities,left_on='city_id',right_on='id',suffixes=('', '_x'),h
 df = df.loc[:,~df.columns.duplicated()]
 df =  pd.merge(df, df_goals, left_on='kind',right_on='kind_1',suffixes=('', '_x'),how = 'inner')
 df = df.loc[:,~df.columns.duplicated()]
-df = df[['id','touchpointable_id','kind','title','name','createable_for_name','city_name','description']].copy()    
+df = df[['id','touchpointable_id','kind','title','name','createable_for_name','description']].copy()    
 df_T =  pd.merge(df, df_interest, left_on='name',right_on='Interest',suffixes=('', '_x'),how = 'inner')
 df_T = df_T.loc[:,~df_T.columns.duplicated()]
 df_T['idx'] = df_T.groupby(['touchpointable_id', 'name']).cumcount()
