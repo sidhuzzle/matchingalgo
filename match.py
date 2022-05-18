711801ced72ad9ae7c2369b9bb633321dfdf4e8b
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-05-18 23:52:05 +0530

    Update match.py

--- match.py
+++ match.py
@@ -3,7 +3,8 @@ import pandas as pd
 import psycopg2 as pg
 import collections, functools, operator
 import numpy as np
-st.cache(ttl=72*7200)
+st.cache(ttl=24*3600)
+st.experimental_memo(suppress_st_warning=True)
 engine = pg.connect("dbname='huzzle_staging' user='postgres' host='huzzle-staging.ct4mk1ahmp9p.eu-central-1.rds.amazonaws.com' port='5432' password='2Yw*PG9x-FcWvc7R'")
 df_tags = pd.read_sql('select * from tags', con=engine)
 df_degrees = pd.read_sql('select * from degrees', con=engine)
