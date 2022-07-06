e8308e491aa24bae519790117d180a4284eead34
sidhuzzle <104455849+sidhuzzle@users.noreply.github.com>
2022-07-07 03:09:16 +0530

    Update match.py

--- match.py
+++ match.py
@@ -57,7 +57,7 @@ Subject = st.selectbox('Enter the subject',df_subjects['name'].unique(),key = 'f
 Degree =  st.selectbox('Enter the degree',df_degrees['name'].unique(),key = 'six')
 year = ['First Year ','Second Year','Third Year','Final Year']
 Year = st.selectbox('Enter the year',year,key = 'seven')
-submit_button = st.button('Submit',key = 'eight')
+
 goals_1 =  pd.DataFrame(goals,columns =['Goals'])
 df_goals = pd.merge(df_goals, goals_1, left_on='title',right_on='Goals',suffixes=('', '_x'),how = 'inner')
 df_goals = df_goals.loc[:,~df_goals.columns.duplicated()]
@@ -507,6 +507,8 @@ Jobs = group_12.get_group("Job")
 Jobs =  pd.merge(df, Jobs, left_on='id',right_on='id',suffixes=('', '_x'),how = 'inner')
 Jobs = Jobs.loc[:,~Jobs.columns.duplicated()]
 Jobs = Jobs.iloc[:20]
-st.write(Internship)
-st.write(Events)
-st.write(Jobs)
+if st.button('Submit',key = eight):
+  
+    st.write(Internship)
+    st.write(Events)
+    st.write(Jobs)
